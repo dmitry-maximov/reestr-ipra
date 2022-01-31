7bc2ca382d45408f757d3be070d12820d47c8089
Dmitry <maximow-dimka@yandex.ru>
2022-01-31 15:32:06 +0300

    Add a temporary navigation bar

--- client/src/pages/Reestr.jsx
+++ client/src/pages/Reestr.jsx
@@ -1,4 +1,16 @@
+import React, { useEffect, useState } from 'react';
+import { fetchOrganizations } from '../api/organizationAPI';
+
 function Reestr() {
+  const [orgs, setOrgs] = useState([]);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => async () => {
+    const response = await fetchOrganizations(limit, page);
+    setOrgs([...orgs, ...response.data]);
+  });
+
   return (
     <>
       <h1>Страница реестра</h1>

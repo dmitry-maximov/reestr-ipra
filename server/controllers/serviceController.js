d2ff1aa9071df646113c8243458c0c4a2a04c550
Dmitry <maximow-dimka@yandex.ru>
2022-02-10 11:17:36 +0300

    added models and controllers for service and type entites

---
+++ server/controllers/serviceController.js
@@ -0,0 +1,44 @@
+const ApiError = require('../handlers/apiError');
+const { Service } = require('../models/index');
+
+class ServiceController {
+  async create(req, res, next) {
+    try {
+      const { name, description } = req.body;
+      const createdServices = await Service.create({
+        name,
+        description,
+      });
+      return res.json(createdServices);
+    } catch (err) {
+      return next(ApiError.badRequest(err));
+    }
+  }
+
+  async getAll(req, res) {
+    let { limit } = req.query;
+    limit = limit || 9;
+    let listServices;
+
+    if (limit > 0)
+      listServices = await Service.findAndCountAll({
+        limit,
+      });
+    else listServices = await Service.findAndCountAll();
+    return res.json(listServices);
+  }
+
+  async getOne(req, res, next) {
+    //TO DO:
+  }
+
+  async delete(req, res, next) {
+    //TO DO:
+  }
+
+  async update(req, res, next) {
+    //TO DO:
+  }
+}
+
+module.exports = new ServiceController();
--- server/models/index.js
+++ server/models/index.js
@@ -1,22 +1,46 @@
-const sequelize = require("../db");
-const { DataTypes } = require("sequelize");
+const sequelize = require('../db');
+const { DataTypes } = require('sequelize');
 
-const Organization = sequelize.define("organization", {
+const Organization = sequelize.define('organization', {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   name: { type: DataTypes.STRING, unique: true, allowNull: false },
   addres: { type: DataTypes.STRING },
   phone: { type: DataTypes.STRING },
 });
 
-const OrganizationInfo = sequelize.define("organization_info", {
+const OrganizationInfo = sequelize.define('organization_info', {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   full_name: { type: DataTypes.STRING },
 });
 
-Organization.hasOne(OrganizationInfo, { as: "info" });
+const Service = sequelize.define('service', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, unique: true, allowNull: false },
+  description: { type: DataTypes.TEXT },
+});
+
+const Type = sequelize.define('type', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, unique: true, allowNull: false },
+});
+
+const OrganizationService = sequelize.define('organization_service', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+});
+
+Organization.hasOne(OrganizationInfo, { as: 'info' });
 OrganizationInfo.belongsTo(Organization);
+
+Type.hasMany(Service);
+Service.belongsTo(Type);
+
+Organization.belongsToMany(Service, { through: OrganizationService });
+Service.belongsToMany(Organization, { through: OrganizationService });
 
 module.exports = {
   Organization,
   OrganizationInfo,
+  Service,
+  Type,
+  OrganizationService,
 };

ec03ab0fb2fd20778ded323963323a2f0d280f08
Dmitry <maximow-dimka@yandex.ru>
2022-02-21 15:24:49 +0300

    added model user and added services for registration and login

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,9 +3,11 @@ const router = new Router();
 const organizationRouter = require('./organizationRouter');
 const serviceRouter = require('./serviceRouter');
 const typeRouter = require('./typeRouter');
+const authRouter = require('./userRouter');
 
 router.use('/organization', organizationRouter);
 router.use('/service', serviceRouter);
 router.use('/type', typeRouter);
+router.use('/auth', authRouter);
 
 module.exports = router;
